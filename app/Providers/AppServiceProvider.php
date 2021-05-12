96676b183159c72c38a74d0b607801abbb1ac374
Inas Azizah <inasazizah64@gmail.com>
2021-05-12 17:10:56 +0700

    Fix schema default string

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Schema;
 use Inertia\Inertia;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	Schema::defaultStringLength(191);	
+
         Inertia::share([
             'errors' => function () {
                 return Session::get('errors')
