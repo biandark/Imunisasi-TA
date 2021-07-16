7883080e1165875fa77fb73f2ad3dfdc43ccb151
biandark <61008049+biandark@users.noreply.github.com>
2021-07-16 17:03:18 +0700

    update

--- app/Console/Commands/SendWA.php
+++ app/Console/Commands/SendWA.php
@@ -41,7 +41,6 @@ class SendWA extends Command
      */
     public function handle()
     {
-        $awal = microtime(true);
         $reminder = new WAreminder();
 
         $dayafter = date('Y-m-d', strtotime( "+2 days" ));
@@ -74,9 +73,6 @@ class SendWA extends Command
            
         }
 
-        $akhir = microtime(true);
-        $lama = $akhir - $awal;
-        echo "<p>Lama eksekusi script adalah: ".$lama." microsecond</p>";
         $this->info('Success');
     }
 }
